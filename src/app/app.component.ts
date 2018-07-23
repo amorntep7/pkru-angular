a30bf925a51e49794957f0a7f522cb2f84b899b9
amorntep7 <s5810886137@pkru.ac.th>
2018-07-23 13:33:38 +0700

    init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title:String = 'Got';
+  VERSION:Number = 1.0;
+  disabled:Boolean = true;
+
+  changetitle(event){
+    console.log(event.target.value);
+    this.title=event.target.value;
+    //this.title = event.key;
+  }
+}
