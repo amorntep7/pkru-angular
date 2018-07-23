3102859f369b705407062726fba3be78e1032528
amorntep7 <s5810886137@pkru.ac.th>
2018-07-23 14:10:18 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title:String = 'Got';
   VERSION:Number = 1.0;
-  disabled:Boolean = true;
+  disabled:Boolean = false;
 
   changetitle(event){
     console.log(event.target.value);
